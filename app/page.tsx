4942d995f94f4c934fb2903bf4774320a5e2562e
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-12-08 23:50:13 -0500

    removed quotes

--- app/page.tsx
+++ app/page.tsx
@@ -17,20 +17,26 @@ export default function Home() {
 
     gsap.fromTo(".page-title h1", 
       { opacity: 0 }, 
-      { opacity: 1, duration: 2, delay: 1, scale: 1.5 }
+      { opacity: 1, 
+        duration: 2, 
+        delay: 1, 
+        scale: 1.5 
+      }
     );
 
     gsap.fromTo(".sample-info h3", 
       { opacity: 0 }, 
-      { opacity: 1, duration: 2, delay: 2 }
+      { opacity: 1, 
+        duration: 2, 
+        delay: 2 }
     );
 
     gsap.fromTo(".sample-info p", 
-    { opacity: 0 }, 
-    { opacity: 1, duration: 2.5, delay: 2.15 }
-  );
-
-
+      { opacity: 0 }, 
+      { opacity: 1, 
+        duration: 2.5, 
+        delay: 2.15 }
+    );
   }, []);
 
 
@@ -38,15 +44,15 @@ export default function Home() {
     <main className="flex flex-col min-h-screen items-center justify-center">
       <div className="page-title flex justify-center">
         <h1 className="text-4xl font-bold opacity-0">kr1st0-beats</h1>
-      </div>  
+      </div>
       <div className="flex items-center justify-center box mb-10 h-80 w-80 bg-cyan-300 rounded-lg">
         <MediaPlayer />
       </div>
       <div className="sample-info mt-20">
         <h3>Samples Used: </h3>
-        <p><a href="https://www.youtube.com/watch?v=oqAqhgsv410&ab_channel=DavieAllan%26TheArrows-Topic">"Loser's Lament" by <span className="text-sky-400">Davie Allen & the Arrows</span></a></p>
-        <p>String Gourd Instrument from Allan Lomax's Songs of Thailand</p>
-        <p>Alan Watts' "Limitations of Language"</p>
+        <p><a href="https://www.youtube.com/watch?v=oqAqhgsv410&ab_channel=DavieAllan%26TheArrows-Topic">&#39;Loser's Lament&#39; by <span className="text-sky-400">Davie Allen & the Arrows</span></a></p>
+        <p>String Gourd Instrument from Allan Lomax&#34;s Songs of Thailand</p>
+        <p>Alan Watts&#34; &#39;Limitations of Language&#39;</p>
         <p>Various Clips from TV Advertisements</p>
         <p>All instruments and production by <a href="http://kristo-portfolio.vercel.app"><span className="text-sky-400">Kr1st0</span></a></p>
       </div>
