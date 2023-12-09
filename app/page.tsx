46f127e9fc2e713df9326fe05c04dc3bd184584f
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-12-08 22:08:02 -0500

    bigger update

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,22 @@ export default function Home() {
   useEffect(() => {
     gsap.to(".box", { 
       rotate: 360,
-      y: 200,
+      y: 150,
       duration: 2,
-    });  });
+    });
+
+    gsap.fromTo(".page-title h1", 
+      { opacity: 0 }, 
+      { opacity: 1, duration: 2, delay: 1 }
+    );
+  }, []);
+
 
   return (
-    <main className="flex min-h-screen flex-col items-center">
+    <main className="flex flex-col min-h-screen items-center justify-center">
+      <div className="page-title flex justify-center">
+        <h1 className="text-4xl font-bold opacity-0">kr1st0-beats</h1>
+      </div>  
       <div className="flex items-center justify-center box mb-10 h-80 w-80 bg-cyan-300 rounded-lg">
         <MediaPlayer />
       </div>
