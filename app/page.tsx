7515c6bf2bcd2eede90365fc9f8d5300e085cb34
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-12-08 23:51:59 -0500

    small change

--- app/page.tsx
+++ app/page.tsx
@@ -37,20 +37,22 @@ export default function Home() {
         duration: 2.5, 
         delay: 2.15 }
     );
+
+    gsap.to('.title-words', {color:'green', duration:0.1, repeat:1, yoyo:true, repeatDelay:1, stagger:0.05 })
   }, []);
 
 
   return (
     <main className="flex flex-col min-h-screen items-center justify-center">
       <div className="page-title flex justify-center">
-        <h1 className="text-4xl font-bold opacity-0">kr1st0-beats</h1>
+        <h1 className="title-words text-4xl font-bold opacity-0">kr1st0-beats</h1>
       </div>
       <div className="flex items-center justify-center box mb-10 h-80 w-80 bg-cyan-300 rounded-lg">
         <MediaPlayer />
       </div>
       <div className="sample-info mt-20">
         <h3>Samples Used: </h3>
-        <p><a href="https://www.youtube.com/watch?v=oqAqhgsv410&ab_channel=DavieAllan%26TheArrows-Topic">&#39;Loser's Lament&#39; by <span className="text-sky-400">Davie Allen & the Arrows</span></a></p>
+        <p><a href="https://www.youtube.com/watch?v=oqAqhgsv410&ab_channel=DavieAllan%26TheArrows-Topic">&#39;Loser&#34;s Lament&#39; by <span className="text-sky-400">Davie Allen & the Arrows</span></a></p>
         <p>String Gourd Instrument from Allan Lomax&#34;s Songs of Thailand</p>
         <p>Alan Watts&#34; &#39;Limitations of Language&#39;</p>
         <p>Various Clips from TV Advertisements</p>
