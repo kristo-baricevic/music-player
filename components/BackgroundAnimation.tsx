b864ef44a4b4212976341672cfd0f92e30aefb74
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-12-09 14:51:05 -0500

    component setup

---
+++ components/BackgroundAnimation.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+
+type BackgroundAnimationProps = {};
+
+const BackgroundAnimation: React.FC<BackgroundAnimationProps> = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+
+  }, []);
+
+  return (
+   <div>
+
+   </div>
+  
+  );
+};
+
+export default BackgroundAnimation;
