9468c50a451986a91d2545a8312f11d654326d71
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2024-01-02 17:38:24 -0500

    body parser

--- app/backend/server.js
+++ app/backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.json()); // for parsing application/json
+app.use(express.json()); 
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -15,11 +15,11 @@ app.listen(port, () => {
 app.get('/api/songs', (req, res) => {
     // Logic to fetch songs
     res.json({ songs: ['song1', 'song2', 'song3'] });
-  });
+});
 
-  app.use((err, req, res, next) => {
+app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
-  });
+});
   
   
