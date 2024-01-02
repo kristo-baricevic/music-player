a077a07c242ad07eff8e5134c1296fd6030746bb
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2024-01-02 17:36:54 -0500

    added backend

---
+++ app/backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json()); // for parsing application/json
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+app.get('/api/songs', (req, res) => {
+    // Logic to fetch songs
+    res.json({ songs: ['song1', 'song2', 'song3'] });
+  });
+
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+  
+  
