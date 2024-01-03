d2e2f9a259c9f07aa7ef8c06803b50c2a1c8442e
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2024-01-03 11:25:10 -0500

    updated folder structure

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,15 +1,23 @@
 const express = require('express');
+
 const app = express();
+
 const port = 3000;
 
 app.use(express.json()); 
 
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+
+app.use('/static', express.static('storage'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
 
 app.get('/api/songs', (req, res) => {
